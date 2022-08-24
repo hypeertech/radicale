5d21783c0ce45dd5bb013ce5220844fcf53c7c74
Alexander Bukhalo <alex@hypeer.company>
2022-08-24 10:15:48 +0300

    support authentication with system user on openbsd ref: https://github.com/Kozea/Radicale/pull/1175

--- radicale/auth/__init__.py
+++ radicale/auth/__init__.py
@@ -33,7 +33,7 @@ from typing import Sequence, Tuple, Union
 from radicale import config, types, utils
 
 INTERNAL_TYPES: Sequence[str] = ("none", "remote_user", "http_x_remote_user",
-                                 "htpasswd")
+                                 "htpasswd", "bsdauth")
 
 
 def load(configuration: "config.Configuration") -> "BaseAuth":
